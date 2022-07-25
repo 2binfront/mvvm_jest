af32afd0f459bfa22fcd05796128662e63088469
2binfront <824225714@qq.com>
2022-07-25 15:00:08 +0800

    little change

--- lightfulMVVM/__tests__/test_all.ts
+++ lightfulMVVM/__tests__/test_all.ts
@@ -39,5 +39,36 @@ describe("global test", () => {
             }
         );
     });
-    
+    test("click is converted to touchstart in a mobile environment", () => {
+        Reflect.defineProperty(window.navigator, "userAgent", {
+          value: "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1",
+          writable: true,
+          enumerable: false,
+          configurable: true,
+        });
+        expect(() => {
+          const view = document.createElement("div");
+          view.setAttribute('id','app')
+          view.innerHTML = "<div @click='show'><div>";
+          new Vue({
+            el:'#app',
+            data: {},
+            methods:{}
+          });
+        }).not.toThrowError();
+      });
+    test("throw the error correctly when Select v-model is it an array according to the attribute 'multi'", () => {
+        expect(() => {
+          const view = document.createElement("div");
+          view.setAttribute('id','app')
+          view.innerHTML = "<select v-model='fruit'></select>";
+          new Vue({
+            el:"",
+            data: {
+              fruit: [],
+            },
+            methods:{}
+          });
+        }).toThrowError();
+      });
   });
